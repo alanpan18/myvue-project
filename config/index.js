6103895f7def7bd7c6b47951c89c5897f1efb079
alanpan18 <2351963493@qq.com>
2018-11-21 23:04:22 +0800

    vue_demo 改造升级

--- config/index.js
+++ config/index.js
@@ -1,18 +1,18 @@
-var path = require(‘path‘)
+var path = require('path')
 module.exports = {
   // 构建产品时使用的配置
   build: {
     // 环境变量
-    env: require(‘./prod.env‘),
+    env: require('./prod.env'),
 // html入口文件
-index: path.resolve(__dirname, ‘../dist/index.html‘),
+index: path.resolve(__dirname, '../dist/index.html'),
 // 产品文件的存放路径
-assetsRoot: path.resolve(__dirname, ‘../dist‘),
+assetsRoot: path.resolve(__dirname, '../dist'),
 // 二级目录，存放静态资源文件的目录，位于dist文件夹下
-assetsSubDirectory: ‘static‘,
+assetsSubDirectory: 'static',
 // 发布路径，如果构建后的产品文件有用于发布CDN或者放到其他域名的服务器，可以在这里进行设置
 // 设置之后构建的产品文件在注入到index.html中的时候就会带上这里的发布路径
-assetsPublicPath: ‘/‘,
+assetsPublicPath: '/',
 // 是否使用source-map
 productionSourceMap: true,
   // Gzip off by default as many popular static hosts such as
@@ -22,7 +22,7 @@ productionSourceMap: true,
   // 是否开启gzip压缩
   productionGzip: false,
   // gzip模式下需要压缩的文件的扩展名，设置js、css之后就只会对js和css文件进行压缩
-  productionGzipExtensions: [‘js‘, ‘css‘],
+  productionGzipExtensions: ['js', 'css'],
 // Run the build command with an extra argument to
 // View the bundle analyzer report after build finishes:
 // `npm run build --report`
@@ -33,15 +33,15 @@ bundleAnalyzerReport: process.env.npm_config_report
 // 开发过程中使用的配置
 dev: {
   // 环境变量
-  env: require(‘./dev.env‘),
+  env: require('./dev.env'),
   // dev-server监听的端口
   port: 8080,
     // 是否自动打开浏览器
     autoOpenBrowser: true,
     // 静态资源文件夹
-    assetsSubDirectory: ‘static‘,
+    assetsSubDirectory: 'static',
   // 发布路径
-  assetsPublicPath: ‘/‘,
+  assetsPublicPath: '/',
   // 代理配置表，在这里可以配置特定的请求代理到对应的API接口
   // 例如将‘localhost:8080/api/xxx‘代理到‘www.example.com/api/xxx‘
   proxyTable: {
